df9ba11e34b33f24dcdd05b6a74427c832a14ea0
tifenum <boukadidahbib@gmail.com>
2025-05-06 19:11:45 +0100

    g

--- gateway/src/main/java/com/pfe/gateway/controller/AuthController.java
+++ gateway/src/main/java/com/pfe/gateway/controller/AuthController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/auth")
 public class AuthController {
 
     private final KeycloakService keycloakService;
